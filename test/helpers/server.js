1266ab24b1c2f133401897df7ca80a815db377f1
Thiago Lagden <lagden@gmail.com>
2018-02-23 10:31:35 -0300

    Upgrade and improvements
    - prefer async/await
    - change mocha -> ava

---
+++ test/helpers/server.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import request from 'supertest'
+import toPort from 'hash-to-port'
+
+function server(koa) {
+	const hash = (Number(String(Math.random()).split('.')[1]) + Date.now()).toString(26)
+	return request.agent(koa.listen(toPort(hash)))
+}
+
+export default server
