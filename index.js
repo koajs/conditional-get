0e0d804459e1696ee568f0b5c5e9d2242eeaa88d
3imed-jaberi <imed_jebari@hotmail.fr>
2020-09-10 01:04:09 +0200

    better src code with es6+ ✨ ..

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 
-'use strict';
+'use strict'
 
 /**
  * Expose `conditional`.
- */
-
-module.exports = conditional;
-
-/**
+ *
  * Conditional GET support middleware.
  *
  * @return {Function}
  * @api public
  */
 
-function conditional() {
-  return function conditional(ctx, next) {
-    return next().then(() => {
-      if (ctx.fresh) {
-        ctx.status = 304;
-        ctx.body = null;
-      }
-    });
+module.exports = function conditional () {
+  return async function (ctx, next) {
+    await next()
+
+    if (ctx.fresh) {
+      ctx.status = 304
+      ctx.body = null
+    }
   }
 }
